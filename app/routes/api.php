2b57da45af25c6b4ed2cee9f8bceb463da4a8c32
Gustavo <gustavolameirao@gmail.com>
2021-11-21 15:18:19 -0300

    Criação da tabela de usuários e estrutura básica da API, listagem funcionando, começo da transação

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// List users
+Route::get('users', [UserController::class, 'index']);
+
+// List single user
+Route::get('user/{id}', [UserController::class, 'show']);
+
+// Transaction
+Route::post('user/transaction', [UserController::class, 'transaction']);
+
+// Create new user
+// Route::post('user', [UserController::class, 'store']);
+
+// // Update user
+// Route::put('user/{id}', [UserController::class, 'update']);
+
+// // Delete user
+// Route::delete('user/{id}', [UserController::class,'destroy']);
