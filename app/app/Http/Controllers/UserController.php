1b01f06f129ec2ffec22b1f0f3bba300a36c2da5
Gustavo <gustavolameirao@gmail.com>
2022-01-05 18:17:21 -0300

    Transformando a operação transaction em uma DB transaction em users. Removendo transactions2

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Http;
 
+use Illuminate\Support\Facades\DB;
+
 class UserController extends Controller
 {
     /**
@@ -94,7 +96,7 @@ class UserController extends Controller
     /**
     * @OA\Post(
 
-    *  path="/user/transaction2",
+    *  path="/user/transaction",
 
     *  tags={"Users"},
 
@@ -158,7 +160,7 @@ class UserController extends Controller
      * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function transaction2(Request $request)
+    public function transaction(Request $request)
     {
         $payer = User::find($request->payer);
         if ($payer === null) {
@@ -200,130 +202,23 @@ class UserController extends Controller
         if ($response2['message'] != 'Success'){
             return response()->json([
                 'message' => 'Fail 2nd Mock',
-            ]);
+            ], 400);
         }
 
-        $payee->total_value += $request->value;
-        $payee->save();
+        try {
+            DB::transaction(function () use ($request, $payee, $payer) {
+                $payee->total_value += $request->value;
+                $payee->save();
 
-        $payer->total_value -= $request->value;
-        $payer->save();
+                $payer->total_value -= $request->value;
+                $payer->save();
+            });
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error during the transaction.',
+            ], 400);
+        }
 
         return response()->json([new UserResource($payer), new UserResource($payee)], 200);
-    }
-
-    /**
-    * @OA\Post(
-
-    *  path="/user/transaction",
-
-    *  tags={"Users"},
-
-    *  summary="Money transferation between users",
-
-    *  @OA\Parameter(name="value",
-
-    *    in="query",
-
-    *    required=true,
-
-    *    @OA\Schema(type="number")
-
-    *  ),
-
-    *  @OA\Parameter(name="payer",
-
-    *    in="query",
-
-    *    required=true,
-
-    *    @OA\Schema(type="integer")
-
-    *  ),
-
-    *  @OA\Parameter(name="payee",
-
-    *    in="query",
-
-    *    required=true,
-
-    *    @OA\Schema(type="integer")
-
-    *  ),
-
-    *  @OA\Response(response="200",
-
-    *    description="Check success value. If it's true, the transfer is completed, also it returns the payer and payee attributes on a json format. If it's false, something went wrong with the transaction, check message for details.",
-
-    *  ),
-
-    *  @OA\Response(response="404",
-
-    *    description="Payer or payee not found",
-
-    *  ),
-
-    * )
-
-    */
-
-    /**
-     * Transaction
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function transaction(Request $request)
-    {
-        $payer = User::findOrFail($request->payer);
-        $payee = User::findOrFail($request->payee);
-
-        if($payer->type == 'regular') {
-            if($payer->total_value - $request->value >= 0) {
-                $response = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
-                $response->json();
-                //var_dump($response['message']);
-                if ($response['message'] == 'Autorizado') {
-                    $response2 = Http::get('http://o4d9z.mocklab.io/notify');
-                    $response2->json();
-
-                    if ($response2['message'] == 'Success') {
-                        $payee->total_value += $request->value;
-                        $payee->save();
-
-                        $payer->total_value -= $request->value;
-                        $payer->save();
-
-                        return response()->json([
-                            'success' => 'true',
-                            'message' => [new UserResource($payer), new UserResource($payee)],
-                        ]);
-
-                        
-                    } else {
-                        return response()->json([
-                            'success' => 'false',
-                            'message' => 'Fail 2nd Mock',
-                        ]);
-                    }
-                } else {
-                    return response()->json([
-                        'success' => 'false',
-                        'message' => 'Fail 1st Mock',
-                    ]);
-                }
-            } else {
-                return response()->json([
-                    'success' => 'false',
-                    'message' => 'Not enough money',
-                ]);
-            }
-        } else {
-            return response()->json([
-                'success' => 'false',
-                'message' => 'Sellers are not allowed to send money',
-            ]);
-
-        }
     }
 }
--- app/tests/Feature/UserTest.php
+++ app/tests/Feature/UserTest.php
@@ -13,7 +13,7 @@ class UserTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function test_users_list_if_exists_t2()
+    public function test_users_list_if_exists()
     {
         $response = $this->get('/api/users/');
 
@@ -27,14 +27,14 @@ class UserTest extends TestCase
         $response->assertNotFound();
     }
 
-    public function test_users_transaction_not_enough_money_t2()
+    public function test_users_transaction_not_enough_money()
     {
         $response = $this->get('/api/user/1');
         $response->assertOk();
         $response = $this->get('/api/user/2');
         $response->assertOk();
 
-        $response = $this->postJson('/api/user/transaction2', [
+        $response = $this->postJson('/api/user/transaction', [
             'value' => '10000',
             'payer' => 1,
             'payee' => 2
@@ -47,14 +47,14 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function test_users_transaction_seller_cant_send_money_t2()
+    public function test_users_transaction_seller_cant_send_money()
     {
         $response = $this->get('/api/user/1');
         $response->assertOk();
         $response = $this->get('/api/user/2');
         $response->assertOk();
 
-        $response = $this->postJson('/api/user/transaction2', [
+        $response = $this->postJson('/api/user/transaction', [
             'value' => '10',
             'payer' => 2,
             'payee' => 1
@@ -67,95 +67,19 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function test_users_transaction_success_t2()
+    public function test_users_transaction_success()
     {
         $response = $this->get('/api/user/1');
         $response->assertOk();
         $response = $this->get('/api/user/2');
         $response->assertOk();
         
-        $response = $this->postJson('/api/user/transaction2', [
+        $response = $this->postJson('/api/user/transaction', [
             'value' => '10',
             'payer' => 1,
             'payee' => 2
         ]);
 
         $response->assertStatus(200);
-    }
-
-    public function test_users_list_if_exists_t1()
-    {
-        $response = $this->get('/api/users/');
-
-        $response->assertJson(["data" => []]);
-    }
-
-    public function test_user_not_found_t1()
-    {
-        $response = $this->get('/api/user/500');
-
-        $response->assertNotFound();
-    }
-
-    public function test_users_transaction_not_enough_money_t1()
-    {
-        $response = $this->get('/api/user/1');
-        $response->assertOk();
-        $response = $this->get('/api/user/2');
-        $response->assertOk();
-
-        $response = $this->postJson('/api/user/transaction', [
-            'value' => '10000',
-            'payer' => 1,
-            'payee' => 2
-        ]);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'success' => 'false',
-                "message" => "Not enough money"
-        ]);
-    }
-
-    public function test_users_transaction_seller_cant_send_money_t1()
-    {
-        $response = $this->get('/api/user/1');
-        $response->assertOk();
-        $response = $this->get('/api/user/2');
-        $response->assertOk();
-
-        $response = $this->postJson('/api/user/transaction', [
-            'value' => '10',
-            'payer' => 2,
-            'payee' => 1
-        ]);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'success' => 'false',
-                'message' => 'Sellers are not allowed to send money',
-        ]);
-    }
-
-    public function test_users_transaction_success_t1()
-    {
-        $response = $this->get('/api/user/1');
-        $response->assertOk();
-        $response = $this->get('/api/user/2');
-        $response->assertOk();
-
-        $response = $this->postJson('/api/user/transaction', [
-            'value' => '10',
-            'payer' => 1,
-            'payee' => 2
-        ]);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'success' => 'true'
-        ]);
     }
 }
